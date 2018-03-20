a1406527eacea8e9e9d2eaaa9176da4dd97cad41
CreativaSL <CreativaSL@CREATIVASL-PC>
2018-03-20 09:36:01 -0600

    Compra Avance

--- CreativaSL.Web.Ganados/Models/_Compra_Datos.cs
+++ CreativaSL.Web.Ganados/Models/_Compra_Datos.cs
@@ -9,6 +9,7 @@ namespace CreativaSL.Web.Ganados.Models
 {
     public class _Compra_Datos
     {
+        private CultureInfo CultureInfo = new CultureInfo("es-MX");
         /// <summary>
         /// Obtiene los datos a mostar en el modulo COMPRAS->INDEX
         /// </summary>
@@ -74,7 +75,7 @@ namespace CreativaSL.Web.Ganados.Models
             }
         }
 
-        public CompraModels ObtenerInfoTab1(CompraModels Compra)
+        public CompraModels GetCompraPacta(CompraModels Compra)
         {
             object[] parametros =
             {
@@ -82,7 +83,7 @@ namespace CreativaSL.Web.Ganados.Models
             };
 
             SqlDataReader SqlDataR = null;
-            SqlDataR = SqlHelper.ExecuteReader(Compra.Conexion, "spCSLDB_COMPRAS_ObtenerInfoTab1", parametros);
+            SqlDataR = SqlHelper.ExecuteReader(Compra.Conexion, "spCSLDB_COMPRAS_GetCompraPactada", parametros);
             CultureInfo CultureInfo = new CultureInfo("es-MX");
 
             while (SqlDataR.Read())
@@ -95,7 +96,7 @@ namespace CreativaSL.Web.Ganados.Models
             }
             return Compra;
         }
-        public CompraModels ObtenerInfoTab2(CompraModels Compra)
+        public CompraModels GetFierros(CompraModels Compra)
         {
             object[] parametros =
             {
@@ -103,7 +104,7 @@ namespace CreativaSL.Web.Ganados.Models
             };
 
             SqlDataReader SqlDataR = null;
-            SqlDataR = SqlHelper.ExecuteReader(Compra.Conexion, "spCSLDB_COMPRAS_ObtenerInfoTab2", parametros);
+            SqlDataR = SqlHelper.ExecuteReader(Compra.Conexion, "spCSLDB_COMPRAS_GetFierros", parametros);
 
             CatFierroModels Fierro;
 
@@ -119,6 +120,97 @@ namespace CreativaSL.Web.Ganados.Models
                 Compra.ListaFierros.Add(Fierro);
             }
             return Compra;
+        }
+        public CompraModels GetFlete(CompraModels Compra)
+        {
+            object[] parametros =
+            {
+                Compra.IDCompra
+            };
+
+            SqlDataReader SqlDataR = null;
+            SqlDataR = SqlHelper.ExecuteReader(Compra.Conexion, "spCSLDB_COMPRAS_GetFlete", parametros);
+
+            while (SqlDataR.Read())
+            {
+                Compra.GuiaTransito = SqlDataR["guiaTransito"].ToString();
+                Compra.CertZoosanitario = SqlDataR["certZoosanitario"].ToString();
+                Compra.CertTuberculosis = SqlDataR["certTuberculosis"].ToString();
+                Compra.CertBrucelosis = SqlDataR["certBrucelosis"].ToString();
+                Compra.Flete.precioFlete = decimal.Parse(SqlDataR["precioFlete"].ToString(), CultureInfo);
+                Compra.Chofer.Nombre = SqlDataR["nombreChofer"].ToString();
+                Compra.Chofer.ApPaterno = SqlDataR["apPaternoChofer"].ToString();
+                Compra.Chofer.ApMaterno = SqlDataR["apMaternoChofer"].ToString();
+                Compra.Vehiculo.Modelo = SqlDataR["modeloVehiculo"].ToString();
+                Compra.Vehiculo.Placas = SqlDataR["placaVehiculo"].ToString();
+                Compra.TipoVehiculo.Descripcion = SqlDataR["descripcionTipoVehiculo"].ToString();
+                Compra.Trayecto.id_lugarOrigen = SqlDataR["id_lugarOrigen"].ToString();
+                Compra.Trayecto.id_lugarDestino = SqlDataR["id_lugarDestino"].ToString();
+            }
+            return Compra;
+        }
+
+        public List<CatChoferModels> GetChoferes(CompraModels Compra)
+        {
+            try
+            {
+                List<CatChoferModels> ListaChoferes = new List<CatChoferModels>();
+
+                CatChoferModels Chofer = new CatChoferModels
+                {
+                    IDChofer = "0",
+                    Nombre = "SELECCIONE UN PROVEEDOR"
+                };
+                ListaChoferes.Add(Chofer);
+                SqlDataReader dr = null;
+                dr = SqlHelper.ExecuteReader(Compra.Conexion, "spCSLDB_COMPRAS_GetChoferes");
+                while (dr.Read())
+                {
+                    Chofer = new CatChoferModels
+                    {
+                        IDChofer = dr["id_chofer"].ToString(),
+                        Nombre = dr["nombre"].ToString() + " " + dr["apPaterno"].ToString() + " " + dr["apMaterno"].ToString()
+                    };
+                    ListaChoferes.Add(Chofer);
+                }
+                return ListaChoferes;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<CatVehiculoModels> GetVehiculos(CompraModels Compra)
+        {
+            try
+            {
+                List<CatVehiculoModels> ListaVehiculos = new List<CatVehiculoModels>();
+
+                CatVehiculoModels Vehiculo = new CatVehiculoModels
+                {
+                    IDVehiculo = "0",
+                    nombreMarca = "SELECCIONE UN VEHICULOS"
+                };
+                ListaVehiculos.Add(Vehiculo);
+                SqlDataReader dr = null;
+                dr = SqlHelper.ExecuteReader(Compra.Conexion, "spCSLDB_COMPRAS_GetVehiculos");
+                while (dr.Read())
+                {
+                    Vehiculo = new CatVehiculoModels
+                    {
+                        IDVehiculo = dr["id_vehiculo"].ToString(),
+                        nombreMarca =  dr["marca"].ToString() + " " + dr["placa"].ToString(),
+                        Modelo = dr["tipo"].ToString()
+                    };
+                    ListaVehiculos.Add(Vehiculo);
+                }
+                return ListaVehiculos;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public ResponseModel Guardar(CompraModels Compra)
@@ -140,7 +232,7 @@ namespace CreativaSL.Web.Ganados.Models
 
                     SqlDataReader SqlDataR = null;
                     SqlDataR = SqlHelper.ExecuteReader(Compra.Conexion, "spCSLDB_COMPRAS_ObtenerInfoTab1", parametros);
-                    CultureInfo CultureInfo = new CultureInfo("es-MX");
+                    
 
                     while (SqlDataR.Read())
                     {
@@ -163,6 +255,39 @@ namespace CreativaSL.Web.Ganados.Models
             }
 
             return rm;
+        }
+
+
+
+
+        public CompraModels ObtenerLugares(CompraModels CompraModels, int opcion)
+        {
+            try
+            {
+                object[] parametros =
+                {
+                    opcion
+                };
+
+                DataSet Ds = null;
+                Ds = SqlHelper.ExecuteDataset(CompraModels.Conexion, "spCSLDB_COMPRAS_getLugares", parametros);
+
+                if (Ds != null)
+                {
+                    if (Ds.Tables.Count > 0)
+                    {
+                        if (Ds.Tables[0] != null)
+                        {
+                            CompraModels.TablaLugares = Ds.Tables[0];
+                        }
+                    }
+                }
+                return CompraModels;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
