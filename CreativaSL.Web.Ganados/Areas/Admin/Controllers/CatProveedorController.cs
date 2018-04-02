9dba36047ac013d1abf6d4b1373256fdb7723063
Creativa-Server <Creativa-Server@CREATIVASERVER>
2018-04-02 09:59:48 -0500

    Cambios Proveedor

--- CreativaSL.Web.Ganados/Areas/Admin/Controllers/CatProveedorController.cs
+++ CreativaSL.Web.Ganados/Areas/Admin/Controllers/CatProveedorController.cs
@@ -51,17 +51,8 @@ namespace CreativaSL.Web.Ganados.Areas.Admin.Controllers
                 _CatProveedor_Datos ProveedorDatos = new _CatProveedor_Datos();
                 Proveedor.Conexion = Conexion;
                 Proveedor.listaSucursal = ProveedorDatos.obtenerListaSucursales(Proveedor);
-                var listaSucursal = new SelectList(Proveedor.listaSucursal, "IDSucursal", "NombreSucursal");
-                ViewData["cmbSucursal"] =listaSucursal;
-
                 Proveedor.listaTipoProveedor = ProveedorDatos.obtenerListaTipoProveedor(Proveedor);
-                var listaTipoProveedores = new SelectList(Proveedor.listaTipoProveedor, "IDTipoProveedor", "Descripcion");
-                ViewData["cmbTipoProveedor"] = listaTipoProveedores;
-
                 Proveedor.ListaGeneroCMB = ProveedorDatos.ObteneComboCatGenero(Proveedor);
-                var list = new SelectList(Proveedor.ListaGeneroCMB, "IDGenero", "Descripcion");
-                ViewData["cmbGenero"] = list;
-
                 return View(Proveedor);
             }
             catch (Exception ex)
@@ -75,58 +66,136 @@ namespace CreativaSL.Web.Ganados.Areas.Admin.Controllers
 
         // POST: Admin/CatProveedor/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(CatProveedorModels Proveedor)
         {
+            _CatProveedor_Datos ProveedorDatos = new _CatProveedor_Datos();
             try
             {
-                CatProveedorModels Proveedor = new CatProveedorModels();
-                _CatProveedor_Datos ProveedorDatos = new _CatProveedor_Datos();
-                Proveedor.Conexion = Conexion;
-                Proveedor.Opcion = 1;
-                Proveedor.Usuario = User.Identity.Name;
-                Proveedor.NombreRazonSocial = collection["NombreRazonSocial"];
-                Proveedor.RFC = collection["RFC"];
-                Proveedor.IDSucursal = collection["listaSucursal"];
-                Proveedor.IDTipoProveedor = Convert.ToInt32(collection["listaTipoProveedor"]);
-                Proveedor.direccion = collection["direccion"];
-                Proveedor.telefonoCelular = collection["telefonoCelular"];
-                Proveedor.telefonoCasa = collection["telefonoCasa"];
-                Proveedor.correo = collection["correo"];
-                Proveedor.sexo = Convert.ToInt32(collection["ListaGeneroCMB"]);
                 HttpPostedFileBase bannerImage = Request.Files[0] as HttpPostedFileBase;
-                if (bannerImage != null && bannerImage.ContentLength > 0)
+                if (!string.IsNullOrEmpty(bannerImage.FileName))
                 {
-                    Stream s = bannerImage.InputStream;
-                    Bitmap img = new Bitmap(s);
-                    Proveedor.ImgINE = img.ToBase64String(ImageFormat.Png);
-                }
-                HttpPostedFileBase bannerImage2 = Request.Files[1] as HttpPostedFileBase;
-                if (bannerImage2 != null && bannerImage2.ContentLength > 0)
-                {
-                    Stream s = bannerImage2.InputStream;
-                    Bitmap img = new Bitmap(s);
-                    Proveedor.ImgManifestacionFierro = img.ToBase64String(ImageFormat.Png);
-                }
-                Proveedor = ProveedorDatos.AcCatProveedor(Proveedor);
-                if (Proveedor.Completado == true)
-                {
-                    TempData["typemessage"] = "1";
-                    TempData["message"] = "Los datos se guardarón correctamente.";
-                    return RedirectToAction("Index");
+                    if (bannerImage != null && bannerImage.ContentLength > 0)
+                    {
+
+                        Stream s = bannerImage.InputStream;
+                        Bitmap img = new Bitmap(s);
+                        Proveedor.ImgINE = img.ToBase64String(ImageFormat.Png);
+
+                    }
                 }
                 else
                 {
-                    TempData["typemessage"] = "2";
-                    TempData["message"] = "Ocurrio un error al intentar guardar los datos. Intente más tarde.";
-                    return RedirectToAction("Create");
+                    ModelState.AddModelError(string.Empty, "Cargar imagen INE");
+                }
+                HttpPostedFileBase bannerImage2 = Request.Files[1] as HttpPostedFileBase;
+                if (!string.IsNullOrEmpty(bannerImage.FileName))
+                {
+                    if (bannerImage2 != null && bannerImage2.ContentLength > 0)
+                    {
+
+                        Stream s = bannerImage2.InputStream;
+                        Bitmap img = new Bitmap(s);
+                        Proveedor.ImgManifestacionFierro = img.ToBase64String(ImageFormat.Png);
+
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Cargar imagen manifestación fierro");
+                }
+
+
+                if (ModelState.IsValid)
+                {
+                    Proveedor.Conexion = Conexion;
+                    Proveedor.Usuario = User.Identity.Name;
+                    Proveedor.Opcion = 2;
+                    Proveedor = ProveedorDatos.AcCatProveedor(Proveedor);
+                    if (Proveedor.Completado)
+                    {
+                        TempData["typemessage"] = "1";
+                        TempData["message"] = "Los datos se guardaron correctamente.";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        Proveedor.listaSucursal = ProveedorDatos.obtenerListaSucursales(Proveedor);
+                        Proveedor.listaTipoProveedor = ProveedorDatos.obtenerListaTipoProveedor(Proveedor);
+                        Proveedor.ListaGeneroCMB = ProveedorDatos.ObteneComboCatGenero(Proveedor);
+                        TempData["typemessage"] = "2";
+                        TempData["message"] = "Ocurrio un error al intentar guardar los datos. Intente más tarde.";
+                        return View(Proveedor);
+                    }
+                }
+                else
+                {
+                    Proveedor.Conexion = Conexion;
+                    Proveedor.listaSucursal = ProveedorDatos.obtenerListaSucursales(Proveedor);
+                    Proveedor.listaTipoProveedor = ProveedorDatos.obtenerListaTipoProveedor(Proveedor);
+                    Proveedor.ListaGeneroCMB = ProveedorDatos.ObteneComboCatGenero(Proveedor);
+                    return View(Proveedor);
                 }
             }
             catch (Exception ex)
             {
+                Proveedor.Conexion = Conexion;
+                Proveedor.listaSucursal = ProveedorDatos.obtenerListaSucursales(Proveedor);
+                Proveedor.listaTipoProveedor = ProveedorDatos.obtenerListaTipoProveedor(Proveedor);
+                Proveedor.ListaGeneroCMB = ProveedorDatos.ObteneComboCatGenero(Proveedor);
                 TempData["typemessage"] = "2";
                 TempData["message"] = "Ocurrio un error al intentar guardar los datos. Contacte a soporte técnico.";
-                return RedirectToAction("Index");
+                return View(Proveedor);
             }
+            //try
+            //{
+            //    CatProveedorModels Proveedor = new CatProveedorModels();
+            //    _CatProveedor_Datos ProveedorDatos = new _CatProveedor_Datos();
+            //    Proveedor.Conexion = Conexion;
+            //    Proveedor.Opcion = 1;
+            //    Proveedor.Usuario = User.Identity.Name;
+            //    Proveedor.NombreRazonSocial = collection["NombreRazonSocial"];
+            //    Proveedor.RFC = collection["RFC"];
+            //    Proveedor.IDSucursal = collection["listaSucursal"];
+            //    Proveedor.IDTipoProveedor = Convert.ToInt32(collection["listaTipoProveedor"]);
+            //    Proveedor.Direccion = collection["direccion"];
+            //    Proveedor.telefonoCelular = collection["telefonoCelular"];
+            //    Proveedor.telefonoCasa = collection["telefonoCasa"];
+            //    Proveedor.correo = collection["correo"];
+            //    Proveedor.sexo = Convert.ToInt32(collection["ListaGeneroCMB"]);
+            //    HttpPostedFileBase bannerImage = Request.Files[0] as HttpPostedFileBase;
+            //    if (bannerImage != null && bannerImage.ContentLength > 0)
+            //    {
+            //        Stream s = bannerImage.InputStream;
+            //        Bitmap img = new Bitmap(s);
+            //        Proveedor.ImgINE = img.ToBase64String(ImageFormat.Png);
+            //    }
+            //    HttpPostedFileBase bannerImage2 = Request.Files[1] as HttpPostedFileBase;
+            //    if (bannerImage2 != null && bannerImage2.ContentLength > 0)
+            //    {
+            //        Stream s = bannerImage2.InputStream;
+            //        Bitmap img = new Bitmap(s);
+            //        Proveedor.ImgManifestacionFierro = img.ToBase64String(ImageFormat.Png);
+            //    }
+            //    Proveedor = ProveedorDatos.AcCatProveedor(Proveedor);
+            //    if (Proveedor.Completado == true)
+            //    {
+            //        TempData["typemessage"] = "1";
+            //        TempData["message"] = "Los datos se guardarón correctamente.";
+            //        return RedirectToAction("Index");
+            //    }
+            //    else
+            //    {
+            //        TempData["typemessage"] = "2";
+            //        TempData["message"] = "Ocurrio un error al intentar guardar los datos. Intente más tarde.";
+            //        return RedirectToAction("Create");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    TempData["typemessage"] = "2";
+            //    TempData["message"] = "Ocurrio un error al intentar guardar los datos. Contacte a soporte técnico.";
+            //    return RedirectToAction("Index");
+            //}
         }
 
         // GET: Admin/CatProveedor/Edit/5
@@ -179,7 +248,7 @@ namespace CreativaSL.Web.Ganados.Areas.Admin.Controllers
                 Proveedor.RFC = collection["RFC"];
                 Proveedor.IDSucursal = collection["listaSucursal"];
                 Proveedor.IDTipoProveedor = Convert.ToInt32(collection["listaTipoProveedor"]);
-                Proveedor.direccion = collection["direccion"];
+                Proveedor.Direccion = collection["direccion"];
                 Proveedor.telefonoCelular = collection["telefonoCelular"];
                 Proveedor.telefonoCasa = collection["telefonoCasa"];
                 Proveedor.correo = collection["correo"];
@@ -268,5 +337,6 @@ namespace CreativaSL.Web.Ganados.Areas.Admin.Controllers
 
             }
         }
+        
     }
 }
--- CreativaSL.Web.Ganados/Content/js/validaciones/additionals.js
+++ CreativaSL.Web.Ganados/Content/js/validaciones/additionals.js
@@ -38,3 +38,14 @@ $.validator.addMethod("CMBINT", function (value, element) {
         return true;
     }
 }, 'Seleccione un valor del combo.');
+
+$.validator.addMethod("validarImagen", function () {
+    if (document.getElementById("ImgINEE").value === '') {
+        if ((document.getElementById("ImgINEE").value === ''))
+            return false;
+        else
+            return true;
+    }
+    else
+        return true;
+}, 'Debe seleccionar una imagen.');
