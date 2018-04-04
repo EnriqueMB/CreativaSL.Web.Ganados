6a01476b467fd235158c9920c474277ce5c05769
EddCarlos09 <lackke.141727@gmail.com>
2018-04-04 17:04:07 -0500

    Combustible 001

---
+++ CreativaSL.Web.Ganados/Models/_Combos_Datos.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.ApplicationBlocks.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CreativaSL.Web.Ganados.Models
+{
+    public class _Combos_Datos
+    {
+        public List<CatVehiculoModels> ObtenerComboVehiculos(string Conexion, string IdSucursal)
+        {
+            try
+            {
+                List<CatVehiculoModels> Lista = new List<CatVehiculoModels>();
+                CatVehiculoModels Item;
+                SqlDataReader Dr = SqlHelper.ExecuteReader(Conexion, "spCSLDB_Combo_get_CatVehiculo", IdSucursal);
+                while(Dr.Read())
+                {
+                    Item = new CatVehiculoModels();
+                    Item.IDVehiculo = !Dr.IsDBNull(Dr.GetOrdinal("id_vehiculo")) ? Dr.GetString(Dr.GetOrdinal("id_vehiculo")) : string.Empty;
+                    Item.Modelo = !Dr.IsDBNull(Dr.GetOrdinal("Descripcion")) ? Dr.GetString(Dr.GetOrdinal("Descripcion")) : string.Empty;
+                    Lista.Add(Item);
+                }
+                return Lista;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<CatTipoCombustibleModels> ObtenerComboTiposCombustible(string Conexion)
+        {
+            try
+            {
+                List<CatTipoCombustibleModels> Lista = new List<CatTipoCombustibleModels>();
+                CatTipoCombustibleModels Item;
+                SqlDataReader Dr = SqlHelper.ExecuteReader(Conexion, "spCSLDB_Combo_get_CatTipoCombustible");
+                while (Dr.Read())
+                {
+                    Item = new CatTipoCombustibleModels();
+                    Item.IDTipoCombustible = !Dr.IsDBNull(Dr.GetOrdinal("id_tipoCombustible")) ? Dr.GetInt32(Dr.GetOrdinal("id_tipoCombustible")) : 0;
+                    Item.Descripcion = !Dr.IsDBNull(Dr.GetOrdinal("Descripcion")) ? Dr.GetString(Dr.GetOrdinal("Descripcion")) : string.Empty;
+                    Lista.Add(Item);
+                }
+                return Lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<CatSucursalesModels> ObtenerComboSucursales(string Conexion)
+        {
+            try
+            {
+                List<CatSucursalesModels> Lista = new List<CatSucursalesModels>();
+                CatSucursalesModels Item;
+                SqlDataReader Dr = SqlHelper.ExecuteReader(Conexion, "spCSLDB_Combo_get_CatSucursal");
+                while (Dr.Read())
+                {
+                    Item = new CatSucursalesModels();
+                    Item.IDSucursal = !Dr.IsDBNull(Dr.GetOrdinal("IDSucursal")) ? Dr.GetString(Dr.GetOrdinal("IDSucursal")) : string.Empty;
+                    Item.NombreSucursal = !Dr.IsDBNull(Dr.GetOrdinal("NombreSucursal")) ? Dr.GetString(Dr.GetOrdinal("NombreSucursal")) : string.Empty;
+                    Lista.Add(Item);
+                }
+                return Lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
