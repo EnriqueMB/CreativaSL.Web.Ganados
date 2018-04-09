c2c7d824c3e65678ebff7e52a0d5b2cddd35e9f7
CreativaSL <CreativaSL@CREATIVASL-PC>
2018-04-09 18:26:53 -0500

    Sucursales Add

--- CreativaSL.Web.Ganados/Models/CatSucursalesModels.cs
+++ CreativaSL.Web.Ganados/Models/CatSucursalesModels.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -15,43 +17,59 @@ namespace CreativaSL.Web.Ganados.Models
             _NombreSucursal = string.Empty;
             Conexion = string.Empty;
             Usuario = string.Empty;
+            MermaPredeterminada = 0;
+            ListaSucursales = new List<CatSucursalesModels>();
+            NombreSucursalMatriz = string.Empty;
+            IDEmpresa = string.Empty;
+            Mensaje = string.Empty;
         }
+
         private string _IDSucursal;
+        private string _IDLugar;
+        private string _Direccion;
+        private string _NombreSucursal;
+
+        [RegularExpression("^[0-9]*$",
+            ErrorMessage = "Solo número enteros positivos.")]
+        [DisplayName("Merma predeterminada")]
+        public decimal MermaPredeterminada { get; set; }
+        public List<CatSucursalesModels> ListaSucursales { get; set; }
+
+        [Required]
+        [StringLength(300, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2} y un maximo de {1}.", MinimumLength = 5)]
+        public string NombreSucursalMatriz { get; set; }
+        public string IDEmpresa { get; set; }
 
         public string IDSucursal
         {
             get { return _IDSucursal; }
             set { _IDSucursal = value; }
         }
-
-        private string _IDLugar;
-
         public string IDLugar
         {
             get { return _IDLugar; }
             set { _IDLugar = value; }
         }
-
-        private string _Direccion;
-
+        [Required]
+        [StringLength(300, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2} y un maximo de {1}.", MinimumLength = 5)]
         public string Direccion
         {
             get { return _Direccion; }
             set { _Direccion = value; }
         }
-        private string _NombreSucursal;
-
+        [Required]
+        [StringLength(300, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2} y un maximo de {1}.", MinimumLength = 5)]
         public string NombreSucursal
         {
             get { return _NombreSucursal; }
             set { _NombreSucursal = value; }
         }
-        public decimal MermaPredeterminada { get; set; }
 
         #region Datos De Control
         public string Conexion { get; set; }
         public int Resultado { get; set; }
         public bool Completado { get; set; }
+        public string Mensaje { get; set; }
         public string Usuario { get; set; }
         public int Opcion { get; set; }
         #endregion
