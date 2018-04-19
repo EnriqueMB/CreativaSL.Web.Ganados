a4b588cf1ab74d7b9fe5a1c20845981cd4350572
F.G. Gamboa <the_king_f14@hotmail.com>
2018-04-18 23:51:56 -0500

    FleteImpuesto

--- CreativaSL.Web.Ganados/Models/FleteImpuestoModels.cs
+++ CreativaSL.Web.Ganados/Models/FleteImpuestoModels.cs
@@ -7,16 +7,22 @@ namespace CreativaSL.Web.Ganados.Models
 {
     public class FleteImpuestoModels
     {
+        public FleteImpuestoModels()
+        {
+            Impuesto = new CFDI_ImpuestoModels();
+            TipoImpuesto = new CFDI_TipoImpuestoModels();
+            TipoFactor = new CFDI_TipoFactorModels();
+            RespuestaAjax = new RespuestaAjax();
+        }
+
         public string IDFleteImpuesto { get; set; }
         public decimal Base { get; set; }
         public decimal TasaCuota { get; set; }
         public decimal Importe { get; set; }
-
         
         public CFDI_ImpuestoModels Impuesto { get; set; }
         public CFDI_TipoImpuestoModels TipoImpuesto { get; set; }
         public CFDI_TipoFactorModels TipoFactor { get; set; }
-
 
         public List<CFDI_ImpuestoModels> ListaImpuesto { get; set; }
         public List<CFDI_TipoImpuestoModels> ListaTipoImpuesto { get; set; }
@@ -26,11 +32,9 @@ namespace CreativaSL.Web.Ganados.Models
         public string IDFlete { get; set; }
 
         #region Datos De Control
+        public RespuestaAjax RespuestaAjax { get; set; }
         public string Conexion { get; set; }
-        public int Resultado { get; set; }
-        public bool Completado { get; set; }
         public string Usuario { get; set; }
-        public int Opcion { get; set; }
         #endregion
     }
 }
