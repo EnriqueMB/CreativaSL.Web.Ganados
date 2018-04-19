982373d83e676f85e5774914328c9cc8eb42e15f
EddCarlos09 <lackke.141727@gmail.com>
2018-04-19 12:53:39 -0500

    EntradasAlmacen

---
+++ CreativaSL.Web.Ganados/ViewModels/EntradaAlmacenDetalleViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CreativaSL.Web.Ganados.ViewModels
+{
+    public class EntradaAlmacenDetalleViewModels
+    {
+
+        private string _IDEntrega;
+
+        public string IDEntrega
+        {
+            get { return _IDEntrega; }
+            set { _IDEntrega = value; }
+        }
+
+        private List<CantidadEntregaViewModels> _ListaDetalle;
+
+        public List<CantidadEntregaViewModels> ListaDetalle
+        {
+            get { return _ListaDetalle; }
+            set { _ListaDetalle = value; }
+        }
+
+    }
+}
