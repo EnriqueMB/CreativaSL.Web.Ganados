9141f64c95a73dd188273d20a6ae8a4596d14692
Eder Ramírez <eder-ramtz@hotmail.com>
2018-04-19 12:08:00 -0500

    ok

--- CreativaSL.Web.Ganados/Areas/Admin/Controllers/AsistenciaEmpleadosController.cs
+++ CreativaSL.Web.Ganados/Areas/Admin/Controllers/AsistenciaEmpleadosController.cs
@@ -10,7 +10,7 @@ using System.Web.Mvc;
 
 namespace CreativaSL.Web.Ganados.Areas.Admin.Controllers
 {
-    public class DatosExcelController : Controller
+    public class AsistenciaEmpleadosController : Controller
     {
         // GET: Admin/DatosExcel
         public ActionResult Index()
