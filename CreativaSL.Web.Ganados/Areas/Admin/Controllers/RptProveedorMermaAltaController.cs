c516fb12a16a889703fa79b8887fa86c5e8f78e3
Eder Ramírez <eder-ramtz@hotmail.com>
2018-05-25 17:19:24 -0500

    cambios-rpt

---
+++ CreativaSL.Web.Ganados/Areas/Admin/Controllers/RptProveedorMermaAltaController.cs
@@ -0,0 +1,107 @@
+﻿using CreativaSL.Web.Ganados.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreativaSL.Web.Ganados.Areas.Admin.Controllers
+{
+    public class RptProveedorMermaAltaController : Controller
+    {
+         string Conexion = ConfigurationManager.AppSettings.Get("strConnection");
+        // GET: Admin/RptProveedorMermaAlta
+        public ActionResult Index()
+        {
+
+            try
+            {
+                RptProveedorMermaAltaModels reporte = new RptProveedorMermaAltaModels();
+                _RptProveedorMermaAlta_Datos reporteDatos = new _RptProveedorMermaAlta_Datos();
+                reporte.Conexion = Conexion;
+                reporte.listaRptProveedorMerma = reporteDatos.obtenerListaProveedoresMermaAlta(reporte);
+                return View(reporte);
+            }
+            catch (Exception ex)
+            {
+                RptProveedorMermaAltaModels Proveedor = new RptProveedorMermaAltaModels();
+                TempData["typemessage"] = "2";
+                TempData["message"] = "No se puede cargar la vista";
+                return View(Proveedor);
+            }
+        }
+
+        // GET: Admin/RptProveedorMermaAlta/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/RptProveedorMermaAlta/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/RptProveedorMermaAlta/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/RptProveedorMermaAlta/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/RptProveedorMermaAlta/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/RptProveedorMermaAlta/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/RptProveedorMermaAlta/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CreativaSL.Web.Ganados/Models/RptProveedorMermaAltaModels.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CreativaSL.Web.Ganados.Models
+{
+    public class RptProveedorMermaAltaModels
+    {
+        public RptProveedorMermaAltaModels() {
+            _nombreProveedor = string.Empty;
+            _merma = 0;
+            _mermaTotal = 0;
+            _listaRptProveedorMerma = new List<RptProveedorMermaAltaModels>();
+            Conexion = string.Empty;
+            Resultado = 0;
+            Completado = false;
+            Opcion = 0;
+            Usuario = string.Empty;
+        }
+        private decimal _toleranciaCompra;
+
+        public decimal toleranciaCompra
+        {
+            get { return _toleranciaCompra; }
+            set { _toleranciaCompra = value; }
+        }
+
+        private string _nombreProveedor;
+
+        public string nombreProveedor
+        {
+            get { return _nombreProveedor; }
+            set { _nombreProveedor = value; }
+        }
+        private int _merma;
+
+        public int merma
+        {
+            get { return _merma; }
+            set { _merma = value; }
+        }
+        private int _mermaTotal;
+
+        public int mermaTotal
+        {
+            get { return _mermaTotal; }
+            set { _mermaTotal = value; }
+        }
+        private List<RptProveedorMermaAltaModels> _listaRptProveedorMerma;
+
+        public List<RptProveedorMermaAltaModels> listaRptProveedorMerma
+        {
+            get { return _listaRptProveedorMerma; }
+            set { _listaRptProveedorMerma = value; }
+        }
+        #region Datos De Control
+        public string Conexion { get; set; }
+        public int Resultado { get; set; }
+        public bool Completado { get; set; }
+        public string Usuario { get; set; }
+        public int Opcion { get; set; }
+        #endregion
+    }
+}
