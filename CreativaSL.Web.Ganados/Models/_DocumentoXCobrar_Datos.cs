b2b25ae0dd9327a82e2fe4cb976af4112f64622c
yadi <yadi@CHIQUITA>
2018-08-15 11:07:38 -0500

    JuanMa

--- CreativaSL.Web.Ganados/Models/_DocumentoXCobrar_Datos.cs
+++ CreativaSL.Web.Ganados/Models/_DocumentoXCobrar_Datos.cs
@@ -628,7 +628,7 @@ namespace CreativaSL.Web.Ganados.Models
                 List<DocumentosPorCobrarDetallePagosModels> Lista = new List<DocumentosPorCobrarDetallePagosModels>();
                 DocumentosPorCobrarDetallePagosModels Item;
                 SqlDataReader dr = null;
-                dr = SqlHelper.ExecuteReader(datos.Conexion, "spCSLDB_DocumentoPorPagar_get_DocumentosDetallesPagos", datos.Id_documentoPorCobrar);
+                dr = SqlHelper.ExecuteReader(datos.Conexion, "spCSLDB_DocumentoPorCobrar_get_DocumentosDetallesPagos", datos.Id_documentoPorCobrar);
                 while (dr.Read())
                 {
                     Item = new DocumentosPorCobrarDetallePagosModels();
@@ -646,6 +646,7 @@ namespace CreativaSL.Web.Ganados.Models
                 throw ex;
             }
         }
+
 
         public DocumentosPorCobrarDetallePagosModels GetDetalleDocumentoPago(DocumentosPorCobrarDetallePagosModels DocumentoPago)
         {
