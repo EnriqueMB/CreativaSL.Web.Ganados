18e1737d7aa82565f3effc8a15670b53cc1463b9
AleXandroS <daniel_thebest_spartan032@hotmail.com>
2019-02-01 09:48:14 -0600

    CatTipoCliente Daniel A. 01-02-2019 (Sin Vistas)

---
+++ CreativaSL.Web.Ganados/Models/CatTipoClienteModels.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.ApplicationBlocks.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CreativaSL.Web.Ganados.Models
+{
+    public class CatTipoClienteModels
+    {
+        public CatTipoClienteModels()
+        {
+            _id_tipoCliente = 0;
+            _descripcion = string.Empty;
+            _listaTIpoCliente = new List<CatTipoClienteModels>();
+            Conexion = string.Empty;
+
+        }
+        private int _id_tipoCliente;
+
+        public int id_tipoCliente
+        {
+            get { return _id_tipoCliente; }
+            set { _id_tipoCliente = value; }
+        }
+        private string _id_sucursal;
+
+        private string _descripcion;
+
+        public string descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+        private List<CatTipoClienteModels> _listaTIpoCliente;
+
+        public List<CatTipoClienteModels> listaTipoCliente
+        {
+            get { return _listaTIpoCliente; }
+            set { _listaTIpoCliente = value; }
+        }
+
+        #region Datos De Control
+        public string Conexion { get; set; }
+        public int Resultado { get; set; }
+        public bool Completado { get; set; }
+        public string Usuario { get; set; }
+        public int Opcion { get; set; }
+        #endregion
+    }
+}
