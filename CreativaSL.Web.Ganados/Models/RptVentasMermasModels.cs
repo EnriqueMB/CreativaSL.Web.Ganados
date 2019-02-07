99bda7257f182a7ed95029d6097c185e86403091
ENRIQUE MENDEZ <enrique_talizman@hotmail.com>
2019-02-07 17:31:54 -0600

    ENRIQUE MENDEZ BALLINA - REPORTE DE NOMINA Y REPORTE DE MERMA VENTA

--- CreativaSL.Web.Ganados/Models/RptVentasMermasModels.cs
+++ CreativaSL.Web.Ganados/Models/RptVentasMermasModels.cs
@@ -13,9 +13,9 @@ namespace CreativaSL.Web.Ganados.Models
             IDSucursal = string.Empty;
             NombreCliente = string.Empty;
             NombreSucursal = string.Empty;
-            FolioVenta = 0;
+            FolioVenta = string.Empty;
             FechaFin = DateTime.Today;
-            FechaInicial = DateTime.Today;
+            FechaInicio = DateTime.Today;
             FechaVenta = DateTime.Today;
             MermaCliente = 0;
             MermaVenta = 0;
@@ -30,25 +30,27 @@ namespace CreativaSL.Web.Ganados.Models
             Conexion = string.Empty;
             _ListaVentaMerma = new List<RptVentasMermasModels>();
             DatosEmpresa = new DatosEmpresaViewModels();
+            LugarFlete = string.Empty;
         }
         public string IDSucursal { get; set; }
         public string NombreSucursal { get; set; }
-        public long FolioVenta { get; set; }
+        public string FolioVenta { get; set; }
         public DateTime FechaVenta { get; set; }
         public string NombreCliente { get; set; }
         public decimal MermaCliente { get; set; }
         public int CabezasHembras { get; set; }
-        public int PesosHembras { get; set; }
+        public decimal PesosHembras { get; set; }
         public int CabezasMachos { get; set; }
-        public int PesosMachos { get; set; }
+        public decimal PesosMachos { get; set; }
         public int TotalCabezas { get; set; }
-        public int TotalPesos { get; set; }
+        public decimal TotalPesos { get; set; }
         public string Flete { get; set; }
         public decimal TotalVenta { get; set; }
         public decimal MermaVenta { get; set; }
-        public DateTime FechaInicial { get; set; }
+        public DateTime FechaInicio { get; set; }
         public DateTime FechaFin { get; set; }
         public string Conexion { get; set; }
+        public string LugarFlete { get; set; }
         private List<RptVentasMermasModels> _ListaVentaMerma;
 
         public List<RptVentasMermasModels> ListaVentaMerma
