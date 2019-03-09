f5f1e7a35469ca15df6f6ad9636b0c9d8d20b953
CreativaSL <CreativaSL@CREATIVASL-PC>
2019-03-08 18:53:03 -0600

    COMPROBANTE DE COMPRA - FGG, FALTA CHECAR EL TELEFONO EN QUE SALE CON & CUANDO NO TIENE

---
+++ CreativaSL.Web.Ganados/Models/ComprobanteCompraDetallesOtrasPercepciones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CreativaSL.Web.Ganados.Models
+{
+    public class ComprobanteCompraDetallesOtrasPercepciones
+    {
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Impuesto { get; set; }
+        public decimal Total { get; set; }
+    }    
+}
