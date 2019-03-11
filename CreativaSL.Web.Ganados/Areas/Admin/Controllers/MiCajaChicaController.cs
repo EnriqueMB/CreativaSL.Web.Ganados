89977ac81253779e5e58b1d3e99797f58c09884d
Carlos Higuera <lackke.141727@gmail.com>
2019-03-11 16:18:38 -0600

    Vistas Caja Chica

---
+++ CreativaSL.Web.Ganados/Areas/Admin/Controllers/MiCajaChicaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreativaSL.Web.Ganados.Areas.Admin.Controllers
+{
+    public class MiCajaChicaController : Controller
+    {
+        // GET: Admin/MiCajaChica
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Admin/MiCajaChica
+        public ActionResult Salida()
+        {
+            return View();
+        }
+
+        // GET: Admin/MiCajaChica
+        public ActionResult Entrega()
+        {
+            return View();
+        }
+
+    }
+}
