12a5f905cb9f092050290abe40dc871d98d8896f
ENRIQUE MENDEZ <enrique_talizman@hotmail.com>
2019-03-15 13:22:44 -0600

    Enrique Mendez Ballina

--- CreativaSL.Web.Ganados/Controllers/HomeController.cs
+++ CreativaSL.Web.Ganados/Controllers/HomeController.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Web;
 using System.Web.Mvc;
 
 namespace CreativaSL.Web.Ganados.Controllers
 {
-    public class DefaultController : Controller
+    public class HomeController : Controller
     {
-
-        public RedirectToRouteResult Index()
+        // GET: Home
+        public ActionResult Index()
         {
             return RedirectToRoute(new { controller = "Home", action = "Index" });
+
         }
     }
 }
