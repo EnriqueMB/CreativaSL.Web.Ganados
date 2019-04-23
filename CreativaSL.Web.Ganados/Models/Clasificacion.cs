15dd15ccc3299c6293e26db6c77756ee7975b3e0
Carlos Higuera <lackke.141727@gmail.com>
2019-04-23 16:48:01 -0500

    Catálogo Clasificación de gastos

---
+++ CreativaSL.Web.Ganados/Models/Clasificacion.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CreativaSL.Web.Ganados.Models
+{
+    public class Clasificacion
+    {
+        public int IdTipoClasificacion { get; set; }
+        [Required(ErrorMessage = "Ingrese el nombre de la categoría.")]
+        [Display(Name = "Clasificación")]
+        public string Descripcion { get; set; }
+        public bool SoloLectura { get; set; }
+        public int Orden { get; set; }
+    }
+}
