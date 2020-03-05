ecdf4fd36644b172d25a7607fccdd9c8d08fc5af
F.G. Gamboa <fggamboa@outlook.com>
2020-03-05 12:28:47 -0600

    Se agregar CostoExtra como modulo

---
+++ CreativaSL.Web.Ganados/Areas/Admin/Controllers/CostoExtraController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CreativaSL.Web.Ganados.Areas.Admin.Controllers
+{
+    public class CostoExtraController : Controller
+    {
+        // GET: Admin/CostoExtra
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
